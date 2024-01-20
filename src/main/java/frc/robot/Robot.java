3fd21a63e405c8f179427d984e656a453d3bc482
Connor Godden <connor.godden2006@gmail.com>
2024-01-19 19:55:54 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.lib.Config.Constants;
@@ -16,52 +17,49 @@ import frc.lib.Util.Autonomous;
 import frc.lib.Util.RoboMove;
 
 /**
- * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
+ * This is a demo program showing the use of the DifferentialDrive class,
+ * specifically it contains
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  //Controlling the robot
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
+  // Controlling the robot
+  private Joystick m_driverStick;
+  private Joystick m_operatorStick;
 
   // Parts of the robot drive base
   private DifferentialDrive m_robotDrive;
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+  private final Talon m_leftMotor = new Talon(0);
+  private final Talon m_rightMotor = new Talon(1);
 
-  //Auto choosing
+  // Auto choosing
   private int m_autoSelected;
   private final SendableChooser<Integer> m_chooser = new SendableChooser<>();
   private final SendableChooser<Integer> t_chooser = new SendableChooser<>();
 
   @Override
   public void robotInit() {
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
-
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward.
     m_rightMotor.setInverted(true);
 
     m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    m_driverStick = new Joystick(0);
+    m_operatorStick = new Joystick(1);
     RoboMove.setRobot(m_robotDrive);
 
     // Creates the menus for SmartDashboard
-    m_chooser.setDefaultOption("Drive Forward", 1);
-    m_chooser.addOption("Left Side Red", 2);
-    SmartDashboard.putData("Auto choices", m_chooser);
-    t_chooser.setDefaultOption("Blue", 1);
-    t_chooser.addOption("Red", 2);
-    SmartDashboard.putData("Team", t_chooser);
-    SmartDashboard.putData("Drive Base", m_robotDrive);
+    //m_chooser.setDefaultOption("Drive Forward", 1);
+    //m_chooser.addOption("Left Side Red", 2);
+    //SmartDashboard.putData("Auto choices", m_chooser);
+    //t_chooser.setDefaultOption("Blue", 1);
+    //t_chooser.addOption("Red", 2);
+    //SmartDashboard.putData("Team", t_chooser);
+    //SmartDashboard.putData("Drive Base", m_robotDrive);
   }
-    
 
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+    RoboMove.move(-m_driverStick.getY(), -m_driverStick.getX());
   }
 
   @Override
