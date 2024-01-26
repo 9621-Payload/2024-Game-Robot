a8c7a0ca824027048c6df6557c5d33066cf1a1aa
Connor Godden <connor.godden2006@gmail.com>
2024-01-26 02:49:05 -0500

    Added the subsystems

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+    /* Creates a new Shooter */
+    public Shooter() { }
+
+    /* Movements for the shooter */
+    public void AmpShot() {
+
+    }
+
+    public void SpeakerShot() {
+
+    }
+
+    public void Intake() {
+        
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/TankDrive.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.TankConstants;
+
+public class TankDrive extends SubsystemBase {
+    /* Motor controllers to move the tank */
+    private PWMSparkMax m_leftMotorFollower = new PWMSparkMax(TankConstants.kLeftMotorPort1);
+    private PWMSparkMax m_leftMotors = new PWMSparkMax(TankConstants.kLeftMotorPort2);
+    private PWMSparkMax m_rightMotorFollower = new PWMSparkMax(TankConstants.kRightMotorPort2);
+    private PWMSparkMax m_rightMotors = new PWMSparkMax(TankConstants.kRightMotorPort1);
+
+    /* Robot drive */
+    private final DifferentialDrive m_Drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+
+    /* Gyro Sensor */
+    // private final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
+
+    /* Odometry for tracking robot */
+    // private final DifferentialDriveOdometry m_odometry;
+
+    /* Creates a new TankDrive */
+    public TankDrive() {
+        /* Sets the motors up and add the followers */
+        m_leftMotors.addFollower(m_leftMotorFollower);
+        m_rightMotors.addFollower(m_rightMotorFollower);
+        m_rightMotors.setInverted(true);
+    }
+
+    /**
+     * Drives the robot using arcade controls.
+     */
+    public void Move(DoubleSupplier forward, DoubleSupplier rotation) {
+        m_Drive.arcadeDrive(forward.getAsDouble(), rotation.getAsDouble());
+    }
+}
