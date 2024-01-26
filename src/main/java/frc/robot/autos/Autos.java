56f8a736242a60dcf8f1f0def696430a13bc286a
Connor Godden <connor.godden2006@gmail.com>
2024-01-26 02:52:23 -0500

    Created Auto folder

--- src/main/java/frc/robot/autos/Autos.java
+++ src/main/java/frc/robot/autos/Autos.java
@@ -2,8 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.autos;
 
+import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
