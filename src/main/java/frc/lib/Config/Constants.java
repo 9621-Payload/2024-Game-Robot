c63744336440cd8b6520caf73e15b6b93ed2c90a
Connor Godden <connor.godden2006@gmail.com>
2024-01-26 01:14:35 -0500

    Refresh with Command Base

--- src/main/java/frc/lib/Config/Constants.java
+++
@@ -1,9 +0,0 @@
-package frc.lib.Config;
-
-public class Constants {
-    public final static int LeftMotorPin = 0;
-    public final static int RightMotorPin = 1;
-    public final static int ShooterPin = 2;
-
-    public static int Team = 1;
-}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,63 +5,99 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import frc.lib.Config.Constants;
-import frc.lib.Util.Autonomous;
-import frc.lib.Util.RobotContainer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
-  RobotContainer m_RobotContainer;
+  private Command m_autonomousCommand;
 
-  // Auto choosing
-  private int m_autoSelected;
-  private final SendableChooser<Integer> m_chooser = new SendableChooser<>();
-  private final SendableChooser<Integer> t_chooser = new SendableChooser<>();
+  private RobotContainer m_robotContainer;
 
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
   @Override
   public void robotInit() {
-    m_RobotContainer = new RobotContainer();
-
-
-    // Creates the menus for SmartDashboard
-    //m_chooser.setDefaultOption("Drive Forward", 1);
-    //m_chooser.addOption("Left Side Red", 2);
-    //SmartDashboard.putData("Auto choices", m_chooser);
-    //t_chooser.setDefaultOption("Blue", 1);
-    //t_chooser.addOption("Red", 2);
-    //SmartDashboard.putData("Team", t_chooser);
-    //SmartDashboard.putData("Drive Base", m_robotDrive);
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
   }
 
-  @Override
-  public void teleopPeriodic() {
-  }
-
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
   @Override
   public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
   }
 
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    // Set the team and auto choosen
-    m_autoSelected = m_chooser.getSelected();
-    Constants.Team = t_chooser.getSelected();
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case 1:
-        Autonomous.DriveForward();
-        break;
-      case 2:
-        Autonomous.RightSide();
-        break;
-      default:
-        // Put default auto code here
-        break;
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
   }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
 }
