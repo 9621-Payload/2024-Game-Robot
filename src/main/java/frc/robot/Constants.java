853ad16849f121e2c00aa39799ed3e641d34f652
Connor Godden <connor.godden2006@gmail.com>
2024-01-29 16:34:23 -0500

    Found Constant values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,17 +7,19 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-    public static final int kOperatorControllerPort = 0;
+    public static final int kOperatorControllerPort = 1;
   }
 
   public static class TankConstants {
-    public static final int kLeftMotorPort1 = 0;
-    public static final int kLeftMotorPort2 = 0;
-    public static final int kRightMotorPort1 = 0;
+    public static final int kLeftMotorPort1 = 3;
+    public static final int kLeftMotorPort2 = 2;
+    public static final int kRightMotorPort1 = 1;
     public static final int kRightMotorPort2 = 0;
+    public static final double kDriveMultiplier = 0.9;
   }
 
   public static class ShooterConstants {
-    
+    public static final int kShootMotorUpper = 4;
+    public static final int kShootMotorLower = 5;
   }
 }
