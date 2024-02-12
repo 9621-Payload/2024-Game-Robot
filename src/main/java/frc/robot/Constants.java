f6d3f4268b47f898420e8ad5cc049073992dc73a
9621 Payload <roboticsdurham@gmail.com>
2024-02-12 18:40:52 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,7 +15,7 @@ public final class Constants {
 
   public static class TankConstants {
     public static final int kLeftMotorPort1 = 5;
-    public static final int kLeftMotorPort2 = 7;
+    public static final int SkLeftMotorPort2 = 7;
     public static final int kRightMotorPort1 = 4;
     public static final int kRightMotorPort2 = 6;
     public static final double kDriveMultiplier = 0.8;
